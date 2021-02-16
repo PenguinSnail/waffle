aa522d44e42bd6c7ef805d8064096270813d8d6a
Noah Piraino <penguinsnail@onebuttonmouse.com>
2021-02-15 23:26:05 -0500

    cgm shorthand

--- src/remarks/classic-garfield-moment.js
+++ src/remarks/classic-garfield-moment.js
@@ -3,7 +3,10 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'classic garfield moment',
 	check(message) {
-		return message.content.toLowerCase().includes('classic garfield moment');
+		return (
+			message.content.toLowerCase().includes('classic garfield moment')
+			|| message.content.toLowerCase().includes('cgm')
+		);
 	},
 	execute(message, args) {
 		const file = new Discord.MessageAttachment(__dirname + '/' + '../../assets/remarks/classic-garfield-moment.jpg');
